7895af0e19fb338cbfe356d53928b70add11ad95
Stephen Omoregie <120742265+Cre8steveDev@users.noreply.github.com>
2024-04-26 10:45:48 +0100

    Update SignUpPage.tsx

--- client/src/views/SignUpPage.tsx
+++ client/src/views/SignUpPage.tsx
@@ -1,8 +1,54 @@
 // Implement the SignIn Page
 // Consider using Social Logins Too
+import {useState} from "react";
+
+type TFormData = {
+  full_name:string;
+  email: string;
+  date_of_birth: string;
+  blood_group?: string;
+  genotype?: string;
+  phone_number: string;
+  password: string;
+  gender: string;
+}
 
 const SignUpPage = () => {
-  return <div>SignUpPage</div>;
+  const [formData, setFormData] = useState<TFormData>({})
+
+  // Form States 
+  const [formLoading, setFormLoading] = useState(false);
+  // const [formSuccess, setFormSuccess] = useState(false);
+  const [formError, setFormError] = useState(false);
+
+  const handleFormValueChange = (e)=>{
+    setFormData(form_data=> {...form_data, [e.target.name]: e.target.value})
+  }
+
+  const handleFormSubmit = (e)=>{
+    e.preventDefault();
+    // Perform Form Submission Action
+  }
+  return <div  
+        style={{
+        backgroundImage: "url('/images/bg_services.jpg')",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}>
+    <div>
+    <form method="post">
+      <h1>Register An Account to Use our Full Features</h1>
+      <p>Book Appointments, store chat history and enjoy convenience.</p>
+
+      <input name="full_name" placeholder="Enter Your Full Name" required value={formData.full_name} className="" minLength=8 maxLength=30 />
+      <input name="email" placeholder="Enter Email Address" type="email" required value={formData.email} className="" minLength=8 />
+      <input name="phone_number" placeholder="Enter Phone Number (081234...)" type="tel" required value={formData.phone_number} className="" />
+      <input name="password" placeholder="Use a Secured Password" type="password" required value={formData.full_name} className="" minLength=8 maxLength=30 />
+
+          
+    </form>
+  </div>
+  </div>;
 };
 
 export default SignUpPage;
