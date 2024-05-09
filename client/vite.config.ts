2172ad8f94170dce8f2b0888c0025f40cccca2b0
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-05-09 10:13:24 +0100

    Deployment Branch for testing and presentation

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     },
     host: '0.0.0.0',
     port: 5000,
-    https: false,
+    // https: false,
   },
   // optimizeDeps: {
   //   exclude: ['react-paystack'],
