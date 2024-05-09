0cda3b4cd717930c2c60a2b38e84dc0f4803829e
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-05-09 09:16:18 +0100

    Preparing for Deployment of the Frontend: Medikall

--- client/src/ui/Logo.tsx
+++ client/src/ui/Logo.tsx
@@ -5,7 +5,11 @@ const Logo = () => {
   return (
     <Link to="/">
       <div className="flex items-center gap-2 hover:opacity-75 transition ease-in">
-        <img src="/images/vite.svg" alt="Logo" />
+        <img
+          src="/images/logo.jpg"
+          alt="Logo"
+          className="w-[40px] md:w-[50px] rounded-full"
+        />
         <h2 className="text-2xl font-black text-primary-blue">Medikall</h2>
       </div>
     </Link>
