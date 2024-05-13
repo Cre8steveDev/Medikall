0441364c2247909c8977bf5736c77d475802ce6c
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-05-13 13:28:44 +0100

    Removed trailing slash from cors origin config

--- server/index.ts
+++ server/index.ts
@@ -25,7 +25,7 @@ app.use(cookieParser(secret));
 
 app.use(
   cors({
-    origin: 'https://doc-medikall.onrender.com/',
+    origin: 'https://doc-medikall.onrender.com',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   })
